28d70c10161f89c038a16b620b5ac754c541460e
kasparasb <kasparasb@wix.com>
2025-05-31 07:51:10 +0300

    Updated tabbar

--- components/tab-bar/tab-bar.tsx
+++ components/tab-bar/tab-bar.tsx
@@ -2,7 +2,8 @@ import { cn } from "@/utils/utils";
 import type { BottomTabBarProps } from "@react-navigation/bottom-tabs";
 import { List, Timer } from "lucide-react-native";
 import React from "react";
-import { Pressable, Text, View } from "react-native";
+import { Pressable, View } from "react-native";
+import { gray, rose } from "tailwindcss/colors";
 
 const ROUTES = [
   {
@@ -11,7 +12,7 @@ const ROUTES = [
     path: "index",
   },
   {
-    name: "Goals",
+    name: "Plan",
     Icon: List,
     path: "plan",
   },
@@ -19,7 +20,14 @@ const ROUTES = [
 
 export function TabBar({ state, navigation }: BottomTabBarProps) {
   return (
-    <View className="flex-row bg-orange-500 border-t overflow-hidden border-gray-200 h-16 bottom-10 w-36 rounded-full mx-auto">
+    <View className="flex-row px-[5px] border border-gray-200 bg-gray-200/40 border-t overflow-hidden h-16 bottom-10 w-[104px] rounded-full mx-auto">
+      <View
+        className={cn(
+          "absolute w-12 top-2 bottom-2 bg-rose-200 rounded-full",
+          state.index === 0 && "left-2",
+          state.index === 1 && "right-2"
+        )}
+      />
       {ROUTES.map((route, index) => {
         const isFocused = state.index === index;
 
@@ -27,12 +35,12 @@ export function TabBar({ state, navigation }: BottomTabBarProps) {
           <Pressable
             key={route.path}
             onPress={() => navigation.navigate(route.path)}
-            className={cn("flex-1 items-center justify-center")}
+            className="flex-1 items-center justify-center"
           >
-            <route.Icon color={isFocused ? "#2563eb" : "#4b5563"} />
-            <Text className={isFocused ? "text-blue-600" : "text-gray-600"}>
-              {route.name}
-            </Text>
+            <route.Icon
+              color={isFocused ? rose[500] : gray[600]}
+              strokeWidth={2.5}
+            />
           </Pressable>
         );
       })}
